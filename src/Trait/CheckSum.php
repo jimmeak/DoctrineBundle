c3b6f74be8b46ada7c8b1c268f514625edfdc3a6
Jimmy Found <jimmeak.found@gmail.com>
2023-07-02 09:45:16 +0200

    feat(trait): Checkum trait added

---
+++ src/Trait/CheckSum.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jimmeak\DoctrineBundle\Trait;
+
+use Doctrine\ORM\Mapping as ORM;
+use Jimmeak\DoctrineBundle\Enum\HashAlgorithm;
+
+/**
+ * This trait is meant to use as a storage for hash
+ * made by an algorithm from PHP function hash
+ *
+ * https://www.php.net/manual/en/function.hash
+ *
+ * The longest hash is a string of 128 characters,
+ * which is the max length of the field.
+ */
+trait CheckSum
+{
+    #[ORM\Column(length: 128)]
+    private string $checkSum;
+
+    public function getCheckSum(): string
+    {
+        return $this->checkSum;
+    }
+
+    public function setCheckSum(string $stringOrCheckSum, HashAlgorithm|null $hashAlgorithm = null): static
+    {
+        if ($hashAlgorithm !== null) {
+            $stringOrCheckSum = hash($hashAlgorithm->value, $stringOrCheckSum);
+        }
+
+        $this->checkSum = $stringOrCheckSum;
+        return $this;
+    }
+}
