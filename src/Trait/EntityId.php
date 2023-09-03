9b9273003d94a6b77d0949166c82cb929c8a0a3e
Jimmy Found <jimmeak.found@gmail.com>
2023-09-03 20:53:50 +0200

    chore: Entity Id can take string or Uuid component

--- src/Trait/EntityId.php
+++ src/Trait/EntityId.php
@@ -3,6 +3,7 @@
 namespace Jimmeak\DoctrineBundle\Trait;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
 
 trait EntityId
 {
@@ -14,9 +15,9 @@ trait EntityId
         return $this->entityId;
     }
 
-    public function setEntityId(string $entityId): static
+    public function setEntityId(Uuid|string $entityId): static
     {
-        $this->entityId = $entityId;
+        $this->entityId = $entityId instanceof Uuid ? $entityId->toRfc4122() : $this->entityId;
 
         return $this;
     }
