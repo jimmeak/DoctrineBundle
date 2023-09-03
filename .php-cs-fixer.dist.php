3607247b611d6fd060ad14ec2424d2b458267727
Jimmy Found <jimmeak.found@gmail.com>
2023-09-03 09:50:54 +0200

    feat: PHP 8.2 migration added into php cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,6 +13,7 @@ return $config
     ->setRules([
         '@Symfony' => true,
         '@PSR12' => true,
+        '@PHP82Migration' => true,
         'concat_space' => ['spacing' => 'one'],
         'global_namespace_import' => ['import_classes' => true, 'import_constants' => false, 'import_functions' => false],
         'nullable_type_declaration' => ['syntax' => 'union'],
