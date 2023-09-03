4cc93e0286083c4f4d758836fdabe0afc24e2a74
Jimmy Found <jimmeak.found@gmail.com>
2023-09-03 09:39:20 +0200

    feat: Final settings of PHP CS Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->append([__FILE__]);
+
+$config = new Config();
+
+return $config
+    ->setRules([
+        '@Symfony' => true,
+        '@PSR12' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'global_namespace_import' => ['import_classes' => true, 'import_constants' => false, 'import_functions' => false],
+        'nullable_type_declaration' => ['syntax' => 'union'],
+        'yoda_style' => true,
+    ])
+    ->setFinder($finder);
